6f84fe0353767a2bec9846bbcbc2af3aa40cb57e
Timur Rassolov <timur.rassolov@gmail.com>
2021-04-14 17:23:41 +0300

    server to generate new blocks and receive blockchain

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
-import {printNewGenesisBlockInfo} from './blockchain';
+import * as  bodyParser from 'body-parser';
+import * as express from 'express';
 
-printNewGenesisBlockInfo();
+import {Block, generateNextBlock, getBlockchain} from "./blockchain";
+
+const httpPort: number = parseInt(process.env.CRYPTO_HTTP_PORT) || 3001;
+
+const initHttpServer = () => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        console.log("GET request for /blocks")
+        res.send(getBlockchain());
+
+    });
+
+    app.post('/mineBlock', (req, res) => {
+        console.log("POST request for /mineBlock")
+        const block: Block = generateNextBlock(req.body.data);
+        res.send(block);
+    });
+
+    app.listen(httpPort, () => {
+        console.log('Listening http on port: ' + httpPort);
+    });
+}
+
+initHttpServer();
